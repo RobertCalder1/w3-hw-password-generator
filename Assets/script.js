7e372b5e1582d58449b8c66853a86d2f7d645421
RobertCalder1 <robertcalder27@hotmail.com>
2021-09-08 15:33:25 +1000

    added function for password generation

--- Assets/script.js
+++ Assets/script.js
@@ -96,3 +96,44 @@ var upperCasedCharacters = [
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword () {
+  password.value = "";
+
+    var Characters = prompt("Enter password length:\n(min: 8 max: 128)");
+
+    if (!Characters) {
+    return;
+    }
+  
+    if (Characters < 8) {
+    alert("Password must be 8 or more Characters")
+    return;
+    }
+  
+    if (Characters > 128) {
+    alert("Password must be less than 128 characters")
+    return;
+    }
+  
+    var Uppercase = confirm("Do you want uppercase letters?");
+    var Lowercase = confirm("Do you want lowercase letters?");
+    var Numbers = confirm("Do you want numbers?");
+    var SpecialCharacters = confirm("Do you want special characters?");
+    var UseCharacters = [];
+    //create array depending on selected options
+    if (Uppercase === true) UseCharacters = UseCharacters.concat(upperCasedCharacters);
+    if (Lowercase === true) UseCharacters = UseCharacters.concat(lowerCasedCharacters);
+    if (Numbers === true) UseCharacters = UseCharacters.concat(numericCharacters);
+    if (SpecialCharacters === true) UseCharacters = UseCharacters.concat(specialCharacters);
+    //produce alert if no character options are selected
+    if (UseCharacters.length === 0) {
+    alert("You must select at least one character type!")
+    return;
+    }
+  for (var i = 0; i <= Characters; i++) {
+  var index = Math.floor(Math.random() * UseCharacters.length);
+  password.value += UseCharacters[index];
+    console.log(password.value)
+  }
+}
